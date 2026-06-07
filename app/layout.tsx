592f0d5b604b7c9b35b1e72ebd57851889a03831
Gaddam Bhanu Venkata Abhiram <y22cb020@rvrjc.ac.in>
2026-06-07 22:18:37 +0530

    Integrate Google Analytics in RootLayout
    Added Google Analytics scripts using Next.js Script component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -10,23 +11,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-L49DSL9T79"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-L49DSL9T79');
-</script>
-          <link rel="icon" type="image/webp" href="/favicon.webp"/>
-          <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+        <link rel="icon" type="image/webp" href="/favicon.webp" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <title>Global Academy Embryology</title>
       </head>
-      <body className="bg-white   text-[#1B3A5B]">
+
+      <body className="bg-white text-[#1B3A5B]">
         <Navbar />
-        <main className=" ">{children}</main>
+        <main>{children}</main>
         <Footer />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-L49DSL9T79"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L49DSL9T79');
+          `}
+        </Script>
       </body>
     </html>
   );
